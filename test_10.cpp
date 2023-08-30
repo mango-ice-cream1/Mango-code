147eb8751c4ee3716e5fe05ab325afa440faca4c
mango-ice-cream1 <2677006470@qq.com>
2023-08-30 23:07:19 +0800

    Update test_10.cpp

--- test_10.cpp
+++ test_10.cpp
@@ -1,67 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
 
 #include<stdio.h>
 #include<stdlib.h>
-#include<time.h>
-#include<iostream>
+#include<string.h>
 
-using namespace std;
-
-typedef int KeyType;
-
-typedef struct
-{
-	KeyType key;
-	int* List;
-}ElemType;
-
-
-typedef struct
-{
-	ElemType* R;
-	int length;
-}SSTable;
-
-
-
-//哨兵法减少比较次数，加快速率
-int Search(SSTable S, int key)
-{
-	S.R[0].key = key;
-	int i = 0;
-	for (i = S.length; key != S.R[i].key; i--);
-	return i;
-}
 
 
 int main()
 {
-	int ret = 0;
-	srand((unsigned)time(NULL));
-	SSTable S = { 0 };
-	int n = 16;
-	S.length = n;
-	S.R = (ElemType*)malloc(S.length * sizeof(ElemType));
-	for (int i = 1; i <= S.length-1; i++)
-	{
-		S.R[i].key = rand() % 100 + 1;
-	}
-	for (int i = 1; i <= S.length - 1; i++)
-	{
-		printf("%d ", S.R[i].key);
-	}
-	printf("\n请输入要查找的数字：");
-	cin >> ret;
-	int m=Search(S, ret);
-	if (m != 0)
-	{
-		printf("找到了，在数据中的索引是%d,:)", m);
-	}
-	else
-	{
-		printf("\n没找到！");
-	}
-
+	指针定义前如果未对指针初始化，则需要开辟空间，不能直接对野指针赋值
+	const int n = 10;
+	int arr[n] = { 0 };
+	char** s = NULL;
+	s = (char**)malloc(10 * sizeof(char**));
+	char buff[10] = "hello";
+	s[0] = (char*)malloc(sizeof(buff));
+	strcpy(s[0], buff);
+	printf("%s\n", s[0]);
 	return 0;
 }
+
 
 
